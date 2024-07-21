475983c41b10e958162fd4aaff7b3711e2b853ea
Dor Shtaif <dorshtaif@gmail.com>
2024-07-21 12:56:31 +0300

    make validation of boolean env vars to not crush when given `""` and rather translate that to the default value

--- packages/instrument-info-service/src/utils/env.ts
+++ packages/instrument-info-service/src/utils/env.ts
@@ -3,10 +3,10 @@ import { parseEnv, z, port } from 'znv';
 export { env };
 
 const env = parseEnv(process.env, {
-  DB_LOGGING: z.boolean().default(false),
-  POSTGRES_DB_CONNECTION_URL: z.string().url().min(1),
-  PORT: port().default(3000),
   NODE_ENV: z.enum(['test', 'development', 'production']).default('development'),
-  ENABLE_NGROK_TUNNEL: z.boolean().default(false),
+  PORT: port().default(3000),
+  POSTGRES_DB_CONNECTION_URL: z.string().url().min(1),
+  DB_LOGGING: z.coerce.boolean().default(false),
+  ENABLE_NGROK_TUNNEL: z.coerce.boolean().default(false),
   NGROK_TUNNEL_AUTH_TOKEN: z.string().optional(),
 });
