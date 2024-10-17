510d05516090fc2dabe500c15c2bbc00baffe080
Dor Shtaif <dorshtaif@gmail.com>
2024-10-17 14:47:50 +0300

    improve UI numeric stats handling and display

--- packages/finance-data-app/src/components/PositionsTable/index.tsx
+++ packages/finance-data-app/src/components/PositionsTable/index.tsx
@@ -1,6 +1,6 @@
-import React, { memo, type ReactElement } from 'react';
+import React, { memo, type ReactElement, type ReactNode } from 'react';
 import { range } from 'lodash-es';
-import { Table, Tag, Skeleton } from 'antd';
+import { Table, Tag, Skeleton, Tooltip } from 'antd';
 import { type MaybeAsyncIterable } from 'iterable-operators';
 import { Iterate } from 'react-async-iterable';
 import { LivePriceDisplay } from '../LivePriceDisplay';
@@ -85,7 +85,9 @@ function PositionsTable(props: {
                     <div>
                       <div>
                         <LivePriceDisplay className="" price={pos.marketPrice}>
-                          {marketPrice => `${commonDecimalNumCurrencyFormat(marketPrice, 'USD')}`}
+                          {marketPrice => (
+                            <>{commonDecimalNumCurrencyFormat(marketPrice, pos.currency)}</>
+                          )}
                         </LivePriceDisplay>
                       </div>
                       {pos.timeOfPrice && (
@@ -116,7 +118,7 @@ function PositionsTable(props: {
                 ) : pos.breakEvenPrice === undefined ? (
                   '-'
                 ) : (
-                  `${commonDecimalNumCurrencyFormat(pos.breakEvenPrice, 'USD')}`
+                  <>{commonDecimalNumCurrencyFormat(pos.breakEvenPrice, pos.currency)}</>
                 ),
             },
             {
@@ -129,7 +131,7 @@ function PositionsTable(props: {
                   '-'
                 ) : (
                   <LivePriceDisplay price={pos.unrealizedPnl.percent}>
-                    {revPercent => <>{commonDecimalNumFormat(revPercent)}%</>}
+                    {revPercent => commonPercentageFormat(revPercent)}
                   </LivePriceDisplay>
                 ),
             },
@@ -143,7 +145,7 @@ function PositionsTable(props: {
                   '-'
                 ) : (
                   <LivePriceDisplay price={pos.unrealizedPnl.amount}>
-                    {revAmount => <>{commonDecimalNumCurrencyFormat(revAmount, 'USD')}</>}
+                    {revAmount => <>{commonDecimalNumCurrencyFormat(revAmount, pos.currency)}</>}
                   </LivePriceDisplay>
                 ),
             },
@@ -156,6 +158,7 @@ function PositionsTable(props: {
 
 type HoldingRecord = {
   symbol: string;
+  currency?: string;
   marketPrice?: number;
   timeOfPrice?: Date | string | number;
   marketState?: 'REGULAR' | 'CLOSED' | 'PRE' | 'PREPRE' | 'POST' | 'POSTPOST';
@@ -183,19 +186,39 @@ const CellSkeleton = memo(() => {
 
 const PositionsTableMemo = memo(PositionsTable);
 
-const commonDecimalNumFormat: (num: number) => string = (() => {
+const commonPercentageFormat: (percentage: number) => string = (() => {
   const commonDecimalNumFormatter = new Intl.NumberFormat(undefined, {
+    style: 'percent',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
-  return num => commonDecimalNumFormatter.format(num);
+  return percentage => commonDecimalNumFormatter.format(percentage / 100);
 })();
 
-const commonDecimalNumCurrencyFormat = (num: number, currency: string): string => {
-  return new Intl.NumberFormat(undefined, {
+const commonDecimalNumCurrencyFormat = (amount: number, currencyCode?: string): ReactNode => {
+  const parts = new Intl.NumberFormat(undefined, {
+    ...(currencyCode
+      ? {
+          style: 'currency',
+          currencyDisplay: 'narrowSymbol',
+          currency: currencyCode,
+        }
+      : undefined),
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-    style: 'currency',
-    currency,
-  }).format(num);
+  }).formatToParts(amount);
+
+  const partValues: ReactNode[] = parts.map(p => p.value);
+
+  if (currencyCode) {
+    const currencyPartIdx = parts.findIndex(p => p.type === 'currency');
+    const currencySymbolStr = parts[currencyPartIdx].value;
+    partValues.splice(
+      currencyPartIdx,
+      1,
+      <Tooltip title={() => currencyCode}>{currencySymbolStr}</Tooltip>
+    );
+  }
+
+  return <>{partValues}</>;
 };
