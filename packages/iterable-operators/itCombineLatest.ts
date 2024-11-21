ef6d07e2dda6f5744217990d0a293ac4b74af6b5
Dor Shtaif <dorshtaif@gmail.com>
2024-11-21 14:49:15 +0200

    fix iterable operator `itCombineLatest`

--- packages/iterable-operators/itCombineLatest.ts
+++ packages/iterable-operators/itCombineLatest.ts
@@ -1,56 +1,81 @@
-import { combineLatest as ixCombineLatest } from '@reactivex/ix-esnext-esm/asynciterable';
+import { type ExtractAsyncIterableValue } from './ExtractAsyncIterableValue.js';
 
 export { itCombineLatest };
 
-function itCombineLatest<T, T2>(
-  source: AsyncIterable<T>,
-  source2: AsyncIterable<T2>
-): AsyncIterable<[T, T2]>;
-function itCombineLatest<T, T2, T3>(
-  source: AsyncIterable<T>,
-  source2: AsyncIterable<T2>,
-  source3: AsyncIterable<T3>
-): AsyncIterable<[T, T2, T3]>;
-function itCombineLatest<T, T2, T3, T4>(
-  source: AsyncIterable<T>,
-  source2: AsyncIterable<T2>,
-  source3: AsyncIterable<T3>,
-  source4: AsyncIterable<T4>
-): AsyncIterable<[T, T2, T3, T4]>;
-function itCombineLatest<T, T2, T3, T4, T5>(
-  source: AsyncIterable<T>,
-  source2: AsyncIterable<T2>,
-  source3: AsyncIterable<T3>,
-  source4: AsyncIterable<T4>,
-  source5: AsyncIterable<T5>
-): AsyncIterable<[T, T2, T3, T4, T5]>;
-function itCombineLatest<T, T2, T3, T4, T5, T6>(
-  source: AsyncIterable<T>,
-  source2: AsyncIterable<T2>,
-  source3: AsyncIterable<T3>,
-  source4: AsyncIterable<T4>,
-  source5: AsyncIterable<T5>,
-  source6: AsyncIterable<T6>
-): AsyncIterable<[T, T2, T3, T4, T5, T6]>;
-function itCombineLatest<T>(...sources: AsyncIterable<T>[]): AsyncIterable<T[]>;
-function itCombineLatest<T>(...sources: AsyncIterable<T>[]): AsyncIterable<T[]> {
+function itCombineLatest<TSourceIters extends AsyncIterable<unknown>[]>(
+  ...sources: TSourceIters
+): AsyncIterable<{
+  [I in keyof TSourceIters]: ExtractAsyncIterableValue<TSourceIters[I]>;
+}>;
+function itCombineLatest<T>(...sources: AsyncIterable<T>[]): AsyncIterable<(T | undefined)[]> {
   return {
     [Symbol.asyncIterator]() {
-      const innerIterators = sources.map(source => source[Symbol.asyncIterator]());
+      let remainingActiveItems: SourceStateItem<T>[] = sources.map((source, i) => ({
+        idxInSources: i,
+        iterator: source[Symbol.asyncIterator](),
+        pendingPromise: undefined,
+        mostRecentValue: undefined as any,
+      }));
 
-      const combinedIterator = ixCombineLatest(
-        ...innerIterators.map(it => ({ [Symbol.asyncIterator]: () => it }))
-      )[Symbol.asyncIterator]();
+      const iterator = (async function* () {
+        const latestValues: (T | undefined)[] = new Array(sources.length).fill(undefined);
 
-      return {
-        next: () => {
-          return combinedIterator.next();
-        },
-        return: async () => {
-          await Promise.all(innerIterators.map(it => it.return?.()));
-          return await combinedIterator.return!();
-        },
+        try {
+          await Promise.all(remainingActiveItems.map(itemFetchNextIfNotPending));
+
+          remainingActiveItems = remainingActiveItems.filter(it => !it.mostRecentValue.done);
+
+          remainingActiveItems.forEach(
+            it => (latestValues[it.idxInSources] = it.mostRecentValue.value)
+          );
+
+          if (!remainingActiveItems.length) {
+            return;
+          }
+
+          yield latestValues;
+
+          do {
+            const firstToResolve = await Promise.race(
+              remainingActiveItems.map(itemFetchNextIfNotPending)
+            );
+
+            if (!firstToResolve.mostRecentValue.done) {
+              latestValues[firstToResolve.idxInSources] = firstToResolve.mostRecentValue.value;
+              yield latestValues;
+            } else {
+              remainingActiveItems.splice(remainingActiveItems.indexOf(firstToResolve), 1);
+            }
+          } while (remainingActiveItems.length);
+        } catch (err) {
+          await Promise.all(remainingActiveItems.map(it => it.iterator.return?.()));
+          throw err;
+        }
+      })();
+
+      iterator.return = async function () {
+        await Promise.all(remainingActiveItems.map(it => it.iterator.return?.()));
+        return { done: true, value: undefined };
       };
+
+      return iterator;
     },
   };
 }
+
+function itemFetchNextIfNotPending<T>(item: SourceStateItem<T>): Promise<SourceStateItem<T>> {
+  item.pendingPromise ??= (async () => {
+    const next = await item.iterator.next();
+    item.mostRecentValue = next;
+    item.pendingPromise = undefined;
+    return item;
+  })();
+  return item.pendingPromise;
+}
+
+type SourceStateItem<T> = {
+  idxInSources: number;
+  iterator: AsyncIterator<T>;
+  pendingPromise: undefined | Promise<SourceStateItem<T>>;
+  mostRecentValue: IteratorResult<T>;
+};
