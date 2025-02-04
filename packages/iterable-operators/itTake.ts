b48ce3e1358b5b9c36700bd01258313dac94f2fc
Dor Shtaif <dorshtaif@gmail.com>
2025-02-04 17:15:03 +0200

    slightly optimize `itTake` operator for case of specifying zero as input

--- packages/iterable-operators/itTake.ts
+++ packages/iterable-operators/itTake.ts
@@ -6,6 +6,15 @@ function itTake<T>(count: number): (src: AsyncIterable<T>) => AsyncIterable<T>;
 function itTake<T>(count: number): (src: InfiniteAsyncIterable<T>) => AsyncIterable<T>;
 function itTake<T>(count: number): (src: AsyncIterable<T>) => AsyncIterable<T> {
   return sourceIter => {
+    if (count === 0) {
+      return {
+        [Symbol.asyncIterator]: () => ({
+          next: async () => ({ done: true, value: undefined }),
+          return: async () => ({ done: true, value: undefined }),
+        }),
+      };
+    }
+
     let iterator: AsyncIterator<T>;
     let remainingCount = count;
     let closed = false;
